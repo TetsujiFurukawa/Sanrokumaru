91d691e36048db65ee9a6ecb5c2d77531383c886
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-09-09 00:43:38 +0900

    画面追加中、bootstrapの調整中

--- 02_front/sanrokumaru/src/app/app.module.ts
+++ 02_front/sanrokumaru/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { BodyComponent } from './common/body/body.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { Screen0001LoginComponent } from './screen00/screen0001-login/screen0001-login.component';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent,
+    Screen0001LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, TooltipModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 02_front/sanrokumaru/src/app/screen00/screen0001-login/screen0001-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-screen0001-login',
+  templateUrl: './screen0001-login.component.html',
+  styleUrls: ['./screen0001-login.component.css']
+})
+export class Screen0001LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
