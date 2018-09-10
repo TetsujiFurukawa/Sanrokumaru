565a3c944a9e85c4f0bf147a145378f0e238d47f
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-09-11 00:08:09 +0900

    ログイン作成中

---
+++ 02_front/sanrokumaru/src/app/entity/information.ts
@@ -0,0 +1,5 @@
+export class Information {
+  date: string;
+  information: string;
+
+}
--- 02_front/sanrokumaru/src/app/screen00/screen0001-login/screen0001-login.component.ts
+++ 02_front/sanrokumaru/src/app/screen00/screen0001-login/screen0001-login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { Information } from '../../entity/information';
 
 @Component({
   selector: 'app-screen0001-login',
@@ -16,7 +17,19 @@ export class Screen0001LoginComponent implements OnInit {
   // ログインパスワード
   loginPassword: String;
 
+  // お知らせ
+  informations: Information[];
+
   ngOnInit() {
+    this.setUpInformations();
+
   }
 
+  // Informationsの初期化
+  private setUpInformations() {
+    this.informations = [
+      { date: '2018/8/1', information: '運用停止のお知らせ。' },
+      { date: '2018/2/1', information: '運用開始' },
+    ];
+  }
 }
