776b1309c218b53f5545ca7e159ae30aae700bf0
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-09-23 16:38:36 +0900

    Bootstrap4に変更

--- 02_front/sanrokumaru/src/app/app.module.ts
+++ 02_front/sanrokumaru/src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 import { BodyComponent } from './common/body/body.component';
 import { FooterComponent } from './common/footer/footer.component';
-import { Sc1001LoginComponent } from './sc10/sc1001-login/sc1001-login.component';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
-import { FormsModule } from '@angular/forms';
+import { Sc1001LoginComponent } from './sc10/sc1001-login/sc1001-login.component';
 import { Sc1003EvaluationResultListComponent } from './sc10/sc1003-evaluation-result-list/sc1003-evaluation-result-list.component';
 
 @NgModule({
@@ -22,7 +22,7 @@ import { Sc1003EvaluationResultListComponent } from './sc10/sc1003-evaluation-re
     Sc1003EvaluationResultListComponent
   ],
   imports: [
-    BrowserModule, TooltipModule.forRoot(),
+    BrowserModule, NgbModule.forRoot(),
     FormsModule, AppRoutingModule
   ],
   providers: [],
