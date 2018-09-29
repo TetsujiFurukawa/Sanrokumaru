9266b9840adb4237236846dc8092607f1bcc0529
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-09-30 02:30:13 +0900

    作成中

--- 02_front/sanrokumaru/src/app/sc10/sc1003-evaluation-result-list/sc1003-evaluation-result-list.component.ts
+++ 02_front/sanrokumaru/src/app/sc10/sc1003-evaluation-result-list/sc1003-evaluation-result-list.component.ts
@@ -8,6 +8,20 @@ import { HeaderService } from '../../service/common/header/header.service';
 })
 export class Sc1003EvaluationResultListComponent implements OnInit {
 
+  // 評価対象者
+  selectedEvaluattionTarget = '0';
+  evaluattionTargetData = [
+    { label: '指定なし', value: '0' },
+    { label: '評価対象者のみ表示', value: '1' },
+    { label: '未評価の評価対象者のみ表示(評価残)', value: '2' }
+  ];
+
+  // 退職者
+  selectedRetiree;
+  retireeData = [
+    { label: '退職者を含む', value: '9', selected: false }
+  ];
+
   constructor() {
   }
 
