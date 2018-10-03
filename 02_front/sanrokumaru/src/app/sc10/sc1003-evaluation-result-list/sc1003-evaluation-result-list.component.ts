8ca230907c27b42fb40c9244a279acdda2f9d959
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-10-04 00:58:09 +0900

    カレンダーを組み込み

--- 02_front/sanrokumaru/src/app/sc10/sc1003-evaluation-result-list/sc1003-evaluation-result-list.component.ts
+++ 02_front/sanrokumaru/src/app/sc10/sc1003-evaluation-result-list/sc1003-evaluation-result-list.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import 'bootstrap-datepicker';
+import { EvaluateResultSumaryList } from '../../entity/evaluateResultSumaryList';
+
+// import * as $ from 'jquery';
+
 import { HeaderService } from '../../service/common/header/header.service';
 
 @Component({
@@ -7,6 +12,9 @@ import { HeaderService } from '../../service/common/header/header.service';
   styleUrls: ['./sc1003-evaluation-result-list.component.css']
 })
 export class Sc1003EvaluationResultListComponent implements OnInit {
+
+  searchEvaluationMonthFrom: string;
+  searchEvaluationMonthTo: string;
 
   // 評価対象者
   selectedEvaluattionTarget = '0';
@@ -22,9 +30,72 @@ export class Sc1003EvaluationResultListComponent implements OnInit {
     { label: '退職者を含む', value: '9', selected: false }
   ];
 
+  // 一覧表示用のエンティティ
+  evaluateResultSumaryList: EvaluateResultSumaryList[];
+
+
   constructor() {
   }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.setUpDatePicker();
+    this.setUpEvaluateResultSumaryLisｐ();
+  }
 
+
+  private setUpDatePicker() {
+    $('#datepicker .date').datepicker({
+      locale: 'ja',
+      format: 'yyyy-mm-dd'
+    });
+  }
+
+  // Informationsの初期化
+  private setUpEvaluateResultSumaryLisｐ() {
+    this.evaluateResultSumaryList = [
+      {
+        employeeCode: '12345',
+        employeeName: 'テスト　太郎',
+        employeeDepartment: 'XX営業部',
+        Rank: '10',
+        mailaddress: 'aaa@yahoo.co.jp',
+        totalScore: 720,
+        Score01: 60,
+        Score02: 60,
+        Score03: 60,
+        Score04: 60,
+        Score05: 60,
+        Score06: 60,
+        Score07: 60,
+        Score08: 60,
+        Score09: 60,
+        Score10: 60,
+        Score11: 60,
+        Score12: 60
+      },
+      {
+        employeeCode: '12345',
+        employeeName: 'テスト　次郎',
+        employeeDepartment: 'XX営業部',
+        Rank: '10',
+        mailaddress: 'aaa@yahoo.co.jp',
+        totalScore: 720,
+        Score01: 60,
+        Score02: 60,
+        Score03: 60,
+        Score04: 60,
+        Score05: 60,
+        Score06: 60,
+        Score07: 60,
+        Score08: 60,
+        Score09: 60,
+        Score10: 60,
+        Score11: 60,
+        Score12: 60
+      }
+    ];
+
+
+
+  }
 }
