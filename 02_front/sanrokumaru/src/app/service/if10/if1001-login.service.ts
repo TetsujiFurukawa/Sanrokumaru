76fbfa0edd81fec3694988bd9879804e68a20938
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2018-12-28 16:36:19 +0900

    Creates information service.

--- 02_front/sanrokumaru/src/app/service/if10/if1001-login.service.ts
+++ 02_front/sanrokumaru/src/app/service/if10/if1001-login.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Information } from '../../entity/domain/information';
 import { environment } from '../../../environments/environment';
 
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { AppConst } from '../../app-const';
 
 @Injectable({
   providedIn: 'root'
 })
 export class If1001LoginService {
+  // ヘッダ
+  private headers: any = new Headers({ 'Content-Type': 'application/json' });
+
+  private serverUrl = 'api/if1001';  // Web APIのURL
+
   // お知らせ
-  informations: Information[];
+  private informations: Information[];
 
   // コンストラクタ
   constructor(private http: HttpClient) { }
@@ -18,20 +25,38 @@ export class If1001LoginService {
   /**
    * お知らせを取得する
    */
-  getInformations(): void {
-
+  getInformations(body: any): Observable<Information[]> {
     // サーバ URL
-    const serverUrl = environment.production ? AppConst.URL_PROD_SERVER : AppConst.URL_STG_SERVER;
-    const path = AppConst + 'if1001';
+    // const serverUrl = environment.production ? AppConst.URL_PROD_SERVER : AppConst.URL_STG_SERVER;
+    // const path = AppConst + 'if1001';
 
-    // サーバ URL とパスを結合して URL を生成し GET 通信する
-    this.http.get<Information[]>(serverUrl + path)
-      .toPromise()
-      .then((response) => {
-        this.informations = response;
-      })
-      .catch((error) => {
-        this.informations = null;
-      });
+    return this.http.get<Information[]>(this.serverUrl)
+      .pipe(
+        catchError(this.handleError('getInformations', []))
+      );
+  }
+  /**
+   * 失敗したHttp操作を処理します。
+   * アプリを持続させます。
+   * @param operation - 失敗した操作の名前
+   * @param result - observableな結果として返す任意の値
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: リモート上のロギング基盤にエラーを送信する
+      console.error(error); // かわりにconsoleに出力
+
+      // TODO: ユーザーへの開示のためにエラーの変換処理を改善する
+      this.log(`${operation} failed: ${error.message}`);
+
+      // 空の結果を返して、アプリを持続可能にする
+      return of(result as T);
+    };
+  }
+
+  /** HeroServiceのメッセージをMessageServiceを使って記録 */
+  private log(message: string) {
+    // this.messageService.add(`HeroService: ${message}`);
   }
 }
