cab687347093fc74766210f67589dc885db26df3
TetsujiFurukawa <tetsufuru1968@yahoo.co.jp>
2019-03-23 10:27:48 +0900

    Edit information.service

--- 02_front/sanrokumaru/src/app/service/information/information.service.ts
+++ 02_front/sanrokumaru/src/app/service/information/information.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 import { Information } from 'src/app/entity/domain/information';
-import { INFORMATIONS } from 'src/app/mock/mock-information';
 import { environment } from '../../../environments/environment';
 import { AppConst } from '../../app-const';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -16,16 +15,14 @@ export class InformationService {
   private server = environment.production ? AppConst.URL_PROD_SERVER : AppConst.URL_DEV_SERVER;
   private webApiUrl = 'informations';
 
-  private headers: any = new Headers({ 'Content-Type': 'application/json' });
-
   constructor(private http: HttpClient) { }
 
   getInformations(): Observable<Information[]> {
-    const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-    };
+
     console.log(this.server + this.webApiUrl);
-    return this.http.get<Information[]>(this.server + this.webApiUrl, httpOptions);
+
+    return this.http.get<Information[]>(this.server + this.webApiUrl, AppConst.httpOptions);
+
   }
 
 }
